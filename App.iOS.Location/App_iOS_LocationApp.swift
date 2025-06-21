be897919bf40a44a27ea7278457e7afb48bbb9d3
emrepbu <emrewindowsarg@gmail.com>
2025-06-22 00:03:45 +0300

    Initial Commit

---
+++ App.iOS.Location/App_iOS_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_iOS_LocationApp.swift
+//  App.iOS.Location
+//
+//  Created by emre argana on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct App_iOS_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
